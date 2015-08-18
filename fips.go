24c0a263ad23d0a383be8c9faf37c5cb48300aac
Bryan Davis <bryan@stax.io>
2015-08-17 17:29:37 -0700

    Create fips.go

---
+++ fips.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	lat, long := 47.579681, -122.306896
+
+	fmt.Println(Fips(lat, long))
+}
+
+func Fips(lat, long float64) map[string]interface{} {
+	results := make(map[string]interface{})
+
+	res, err := http.Get(fmt.Sprintf("http://data.fcc.gov/api/block/find?format=json&latitude=%0.5f&longitude=%0.5f&showall=true", lat, long))
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+
+	contents, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+
+	var v interface{}
+	json.Unmarshal(contents, &v)
+
+	results["block"] = v.(map[string]interface{})["Block"].(map[string]interface{})["FIPS"]
+	results["county"] = v.(map[string]interface{})["County"].(map[string]interface{})["FIPS"]
+	results["state"] = v.(map[string]interface{})["State"].(map[string]interface{})["FIPS"]
+
+	return results
+
+}
